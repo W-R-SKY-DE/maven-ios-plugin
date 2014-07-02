e7551747b6d3a03e77aa2a6e29758dde80e57f46
Wolfgang Reithmeier <wolfgang.reithmeier@sky.de>
2014-07-02 11:46:40 +0200

    fixed issue with whitespace in plist-path, changed script

--- src/main/resources/META-INF/write-bundleidentifier.sh
+++ src/main/resources/META-INF/write-bundleidentifier.sh
@@ -4,8 +4,8 @@ SCRIPT_LOCATION=$(cd "$(dirname "$0")"; pwd)
 PLIST_LOCATION=$1
 NEW_BUNDLE_IDENTIFIER=$2
 
-echo "OLD BUNDLE IDENTIFIER=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" $PLIST_LOCATION)"
+echo "OLD BUNDLE IDENTIFIER=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$PLIST_LOCATION")"
 echo "NEW BUNDLE IDENTIFIER= $NEW_BUNDLE_IDENTIFIER"
 
 echo "Write NEW BUNDLE IDENTIFIER into PLIST now"
-/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $NEW_BUNDLE_IDENTIFIER" $PLIST_LOCATION
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $NEW_BUNDLE_IDENTIFIER" "$PLIST_LOCATION"
